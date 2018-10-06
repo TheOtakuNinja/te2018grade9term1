2d67e564dca46578651b3d1b74bd57218c3b998d
Code Next <codenext@Codes-MacBook-Air-2.local>
2018-10-06 13:18:30 -0700

    made sum stuf go boom

--- data.js
+++ data.js
@@ -1,5 +1,26 @@
-// Example
-let message = "Hello World!";
+  // Example
+let message = "(>^_^) Dat's a lota work";
 console.log(message);
 
 // Type your code below!
+let girlName = "Astrid";
+let friendName = "David";
+let myBoiKeith = "Keith";
+let bDayDay = 12;
+let bDayMonth = 11;
+let bDayYear = 2003;
+let likesAnime = true;
+let likesChess = true;
+let findingSomeoneToLove = undefined;
+
+
+
+console.log(girlName);
+console.log(friendName);
+console.log(myBoiKeith);
+console.log(bDayDay);
+console.log(bDayMonth);
+console.log(bDayYear);
+console.log(likesAnime);
+console.log(likesChess);
+console.log(findingSomeoneToLove);
